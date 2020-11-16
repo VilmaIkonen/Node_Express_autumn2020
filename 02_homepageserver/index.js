28bf468dbed6bc6378ab7df607de1ff93d1beadc
VilmaIkonen <vilma.r.ikonen@gmail.com>
2020-11-16 14:44:23 +0200

    02 ready

--- 02_homepageserver/index.js
+++ 02_homepageserver/index.js
@@ -12,7 +12,12 @@ const host = process.env.HOST || 'localhost';
 const server = http.createServer(app);
 
 const homePath = path.join(__dirname, 'home.html');
+const pageBpath = path.join(__dirname, 'pageB.html');
+
+//with this, access to public folder and eg. styles in it.
+app.use(express.static.apply(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.sendFile(homePath));
+app.get('/someotherpage', (req, res) => sendFile(pageBpath));
 
 server.listen(port, host, () => console.log(`Server ${host}: ${port} serving`))
