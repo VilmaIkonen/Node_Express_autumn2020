24a0ab5bf157c4e49349e7501ec267c819552d78
VilmaIkonen <vilma.r.ikonen@gmail.com>
2020-11-28 14:45:38 +0200

    Server created

---
+++ 05_Ikonen_Vilma_turtle_project/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const http = require('http');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const { port, host, storage } = require('./serverConfig.json');
+
+const { turtleDataStorage } = require(path.join(__dirname, storage.storageFolder, storage.dataLayer));
+
+const turtleStorage = turtleDataStorage();
+
+const server = http.createServer(app);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'pages'));
+
+// middlewares
+app.use(express.urlencoded({ extended: false})); // This object will contain key-value pairs, where the value can be a string or array (when extended is false), or any type (when extended is true)
+app.use(express.static(path.join(__dirname, 'public'))); // The root argument specifies the root directory from which to serve static assets
+
+const homePath = path.join(__dirname, 'home.html');
+
+
+// GET methods START //
+
+// To home page
+app.get('/', (req, res) => res.sendFile(homePath));
+
+// GET methods END //
+
+server.listen(port, host, () => console.log(`Server ${host}: ${port} running`));
