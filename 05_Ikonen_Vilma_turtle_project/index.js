028c5b3a70b369a3b87877b319a51285b8a97d1c
VilmaIkonen <vilma.r.ikonen@gmail.com>
2020-12-01 10:20:30 +0200

    Added functionality for updating and removing a turtle

--- 05_Ikonen_Vilma_turtle_project/index.js
+++ 05_Ikonen_Vilma_turtle_project/index.js
@@ -25,16 +25,19 @@ const homePath = path.join(__dirname, 'home.html');
 
 
 // GET and POST START //
+
 // GET route is used to display a new empty form for creating the object. POST is used for saving the info and redirecting 
 
 // To home page
 app.get('/', (req, res) => res.sendFile(homePath));
 
 
-// To all turtles -page
+// GET ALL / To all turtles -page
 app.get('/all', (req, res) => turtleStorage.getAll()
 .then(data => res.render('getall', { result: data.map(turtle => createTurtle(turtle))})))
 
+
+// GET ONE
 // To getone page 
 app.get('/getone', (req, res) => res.render(
   'getone', {
@@ -55,8 +58,9 @@ app.post('/getone', (req, res) => {
   .catch(error => sendErrorPage(res, error)) 
 })
 
+//INSERT
 // Inserting a new turtle to database
-app.get('/inputform', (req, res) => 
+app.get('/insert', (req, res) => 
   res.render('formInsertUpdate', {
     title: 'Insert',
     header: 'Insert new turtle to database',
@@ -78,7 +82,64 @@ app.post('/insert', (req, res) => {
   .catch(error => sendErrorPage(res, error));
 });
 
+//UPDATE
+// Updating turtle data in database
+app.get('/update', (req, res) => 
+  res.render('formInsertUpdate', {
+    title: 'Update',
+    header: 'Get turtle by number and update the data',
+    action: '/update',
+    number: {value: '', readonly: ''},
+    name: {value: '', readonly: 'readonly'},
+    age: {value: '', readonly: 'readonly'},
+    speed: {value: '', readonly: 'readonly'},
+    weightKg: {value: '', readonly: 'readonly'}
+}))
 
+// Posting request from /update page
+app.post('/update', (req, res) => {
+  if(!req.body) res.sendStatus(500);
+
+  turtleStorage.get(req.body.number)
+  .then(turtle => res.render('formInsertUpdate', {
+    title: 'Update',
+    header: 'Get turtle by number and update the data',
+    action: '/updateturtle',
+    number: {value: turtle.number, readonly: 'readonly'},
+    name: {value: turtle.name, readonly: ''},
+    age: {value: turtle.age, readonly: ''},
+    speed: {value: turtle.speed, readonly: ''},
+    weightKg: {value: turtle.weightKg, readonly: ''}
+  }))
+  .catch(error => sendErrorPage(res, error));
+}); 
+
+app.post('/updateturtle', (req, res) => {
+  if(!req.body) res.sendStatus(500);
+
+  else turtleStorage.update(createTurtle(req.body))
+  .then(status => sendStatusPage( res, status))
+  .catch(error => sendErrorPage(res, error))
+})
+
+// REMOVE 
+// Removing turtle from database
+app.get('/remove', (req, res) => 
+  res.render('getone', {
+    title: 'Remove',
+    header: 'Remove a turtle from database based on number',
+    action: '/remove'
+  })
+)
+
+app.post('/remove', (req, res) => {
+  if(!req.body) res.sendStatus(500);
+
+  const turtleNumber = req.body.turtleNumber;
+  turtleStorage.remove(turtleNumber)
+  .then(status => sendStatusPage(res, status))
+  .catch(error => sendErrorPage(res, error));
+});
 
 // GET and POST methods END //
 
