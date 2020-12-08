43bd440d79a46660b570b1457ea91b9df22f9c59
VilmaIkonen <vilma.r.ikonen@gmail.com>
2020-12-08 13:14:47 +0200

    FI entries added and data entry page added

--- 06_formGeneratorDemo/index.js
+++ 06_formGeneratorDemo/index.js
@@ -20,23 +20,80 @@ app.set('views', path.join(__dirname, 'pageviews'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// require fields.json 
+// require fields.json. Is loaded when server starts. Changes done while server is running, are not visible. Need to rerun server to see changes.
 const fields = require('./fields.json');
+const fieldsFI = require('./fieldsFI.json');
 
 // ----- CONFIGURATION END ----- //
 
 // app get routes
 app.get('/', (req, res) => {  
-  res.render('menu', {language: 'en', title: 'Menu', header: 'Menu'})
+  // console.log(req.headers['accept-language']); pure Node version
+  const lang = req.acceptsLanguages('en', 'fi');
+  // console.log(lang);
+  if(lang === 'fi') {
+    res.render('menu', {
+      language: 'fi', 
+      title: 'Valikko', 
+      header: 'Valikko',
+      css: 'menuStyles'
+    })
+  }
+  else {
+    res.render('menu', {
+      language: 'en', 
+      title: 'Menu', 
+      header: 'Menu',
+      css: 'menuStyles'
+    })
+  }  
 })
+
 // getting different language form based on browser language, menu.html parametrized values
 app.route('/en')
   .get((req, res) => res.render('form', {
     language: 'en',
     title: 'Data',
     header: 'Fill',
-    menuLabel: 'Menu'
-}));
+    menuLabel: 'Menu',
+    submitLabel: 'Submit',
+    css: 'styles',
+    // POST action
+    action: '/en', 
+    fields
+    // fields: fields also valid, but not needed in ES6
+}))
+  // .post((req, res) => res.json(req.body));
+  .post((req, res) => res.render('datapage', {
+    language: 'en', 
+    title: 'Data', 
+    header: 'Data',
+    menuLabel:'Menu',
+    css: 'styles',
+    data: req.body    
+  }))
+  
+app.route('/fi')
+  .get((req, res) => res.render('form', {
+    language: 'fi',
+    title: 'Tiedot',
+    header: 'Syötä tiedot',
+    menuLabel: 'Valikko',
+    submitLabel: 'Lähetä',
+    css:'styles',
+    // action for POST
+    action: '/fi', 
+    fields: fieldsFI    
+  }))
+  // .post((req, res) => res.json(req.body));
+  .post((req, res) => res.render('datapage', {
+    language: 'fi', 
+    title: 'Tiedot', 
+    header: 'Syötit tiedot',
+    menuLabel:'Valikko',
+    css: 'styles',
+    data: req.body    
+  }))
 
 
 server.listen(port, host, () => console.log(`Listening ${host}: ${port}`));
--- 06_formGeneratorDemo/readme.md
+++ 06_formGeneratorDemo/readme.md
@@ -8,5 +8,9 @@ pageviews/menu.html (first)
 cut out head from menu.ejs to separate file use the same on all pages
 separate ejs for form
 when making changes to ejs files, check that index.js has the same features (ejs.tags)
+add .post in index.js
+make fields.json for other language (fin)
+create route in ondex for fin version
+parametrization of styles.css
 
 
