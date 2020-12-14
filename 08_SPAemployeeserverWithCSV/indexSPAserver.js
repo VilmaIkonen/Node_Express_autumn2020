e1430acf1dac63d27a3e1b0b7d022f382a16cf14
VilmaIkonen <vilma.r.ikonen@gmail.com>
2020-12-14 15:26:29 +0200

    CSV version for SPAemployeeserver

---
+++ 08_SPAemployeeserverWithCSV/indexSPAserver.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const http = require('http');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const { port, host, storage } = require('./serverConfig.json');
+
+const { createDataStorage } = require(path.join(__dirname, storage.storageFolder, storage.dataLayer));
+
+const dataStorage = createDataStorage();
+
+const server = http.createServer(app);
+
+// middleware 
+// for getting json data in and parsing it.
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// sending home page to server
+const homePath = path.join(__dirname, 'home.html');
+app.get('/', (req, res) => res.sendFile(homePath));
+
+// routes for data getAll, getPerson, remove, add, update. Only page as such is the home.html
+app.get('/all', (req, res) => 
+  dataStorage.getAll()
+  .then(result => res.json(result.map(emp => createPerson(emp)))));
+
+app.post('/getperson', (req, res) => {
+  if(!req.body) res.sendStatus(500);
+
+  const personId = +req.body.personId; // "+" converts to number
+  dataStorage.get(personId)
+  .then(emp => res.json(createPerson(emp)))
+  .catch(error => sendError(res, error))
+})
+
+app.post('/removeperson', (req, res) => {
+  if(!req.body) res.sendStatus(500)
+
+  const personId = +req.body.personId;
+  dataStorage.remove(personId)
+  .then(status => sendStatus(res, status))
+  .catch(error => sendError(res, error))
+})
+
+app.post('/insertperson', (req, res) => {
+  if(!req.body) res.sendStatus(500)
+  dataStorage.insert(createEmployee(req.body))
+  .then(status => sendStatus(res, status))
+  .catch(error => sendError(res, error))
+})
+
+app.post('/updateperson', (req, res) => {
+  if(!req.body) res.sendStatus(500)
+  dataStorage.update(createEmployee(req.body))
+  .then(status => sendStatus(res, status))
+  .catch(error => sendError(res, error))
+})
+
+server.listen(port, host, () => console.log(`Server ${host}: ${port} running`));
+
+function sendError(res, error){
+  sendStatus(res, error)
+}
+
+function sendStatus(res, status) { 
+  return res.json(status);
+}
+
+// Conversion from employee to person
+function createPerson(employee) {
+  return {
+    personId: employee.employeeId, 
+    firstname: employee.firstname,
+    lastname: employee.lastname,
+    department: employee.department,
+    salary: employee.salary
+  }
+}
+
+// conversion from person to employee
+function createEmployee (person) {
+  return {
+    employeeId: person.personId, 
+    firstname: person.firstname,
+    lastname: person.lastname,
+    department: person.department,
+    salary: person.salary
+
+  }
+}
